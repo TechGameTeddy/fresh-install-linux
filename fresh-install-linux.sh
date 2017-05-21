071c629435146addd8d76d2c985d62d3fbe67053
Tedley <tmeralus@gmail.com>
2017-05-20 20:08:51 -0400

    init1

---
+++ fresh-install-linux.sh
@@ -0,0 +1,133 @@
+
+#!/bin/bash 
+# Name: Fresh Install
+# Author: Tedley Meralus 
+# contact: @TechGameTeddy
+#
+# This script is a list of popular tools and utilities 
+# to install inspired by OMG Ubuntu's infamous 
+# Top Things to do after Installing Ubuntu xx.xx articles
+# check them out at http://www.omgubuntu.co.uk/2017/04/things-to-do-after-installing-ubuntu-17-04
+#
+
+
+
+
+
+sudo add-apt-repository ppa:graphics-drivers/ppa 
+sudo apt-get install libxss1 libappindicator1 libindicator7
+sudo add-apt-repository ppa:dawidd0811/neofetch
+sudo add-apt-repository ppa:vlijm/spaceview
+sudo add-apt-repository ppa:gerardpuig/ppa
+sudo add-apt-repository ppa:shutter/ppa
+sudo add-apt-repository ppa:atareao/atareao
+sudo apt-get install shutter
+sudo apt install national-geographic-wallpaper
+sudo apt-get install git
+sudo apt-get install gitg
+sudo apt-get install synaptic
+sudo apt-get install unity-tweak-tool
+sudo apt install google-chrome-stable
+sudo apt-get install steam
+sudo apt-get install transmission
+sudo apt install gimp gimp-gmic
+sudo apt-get install spaceview
+sudo apt-get install ubuntu-cleaner
+sudo apt-get install filezilla
+sudo apt-get install uget
+sudo apt-get install tor
+sudo apt-get install putty
+sudo apt-get install open-vm-tools-desktop
+sudo apt install android-tools-adb android-tools-fastboot
+
+git clone https://github.com/ebruck/indicator-doom-cpu/releases/download/v1.0/indicator-doom-cpu-1.0.0-noarch.deb
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+
+
+##synaptic package manager 
+sudo apt-get install synaptic
+
+##Nvidia drivers
+sudo add-apt-repository ppa:graphics-drivers/ppa 
+
+##Unity tweak tool 
+sudo apt-get install unity-tweak-tool
+
+##Install chrome
+sudo apt install google-chrome-stable
+
+##Install steam 
+sudo apt-get install steam
+
+
+##Transmission
+sudo apt-get install transmission
+
+
+##Neo fetch(system info via terminal)
+sudo add-apt-repository ppa:dawidd0811/neofetch
+sudo apt-get install neofetch
+
+#Install Gimp
+sudo apt install gimp gimp-gmic
+
+
+##Evolution
+sudo apt-get install evolution
+
+ 
+
+
+
+##Install Sublime
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
+
+##install Sublime
+sudo apt-get install sublime-text-installer
+
+
+
+##Install Cinnamon
+# this one fails
+## email the developer and tell them 
+##sudo add-apt-repository ppa:moorkai/cinnamon 
+sudo add-apt-repository ppa:embrosyn/cinnamon
+sudo apt-get install cinnamon
+
+##Nvidia Graphics
+sudo add-apt-repository ppa:graphics-drivers/ppa
+apt-cache search nvidia
+
+##Install SpaceView Indicator
+sudo add-apt-repository ppa:vlijm/spaceview
+sudo apt-get update && sudo apt-get install spaceview
+
+##Install Ubuntu Cleaner
+sudo add-apt-repository ppa:gerardpuig/ppa
+sudo apt-get update && sudo apt-get install ubuntu-cleaner
+
+sudo add-apt-repository ppa:shutter/ppa
+sudo apt-get update
+sudo apt-get install shutter
+
+National Geographic Wallpaper switcher
+sudo add-apt-repository ppa:atareao/atareao
+sudo apt update && sudo apt install national-geographic-wallpaper
+
+
+
+apt-cache search nvidia
+
+
+
+
+
+
+
