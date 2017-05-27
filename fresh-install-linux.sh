27c9db9aafe411acc67513251bf3bb4a382e409a
Tedley <tmeralus@gmail.com>
2017-05-27 09:26:29 -0400

    update
    still cleaning up the peices.

--- fresh-install-linux.sh
+++ fresh-install-linux.sh
@@ -12,85 +12,129 @@
 
 
 
+# function to display menus
+show_menus() {
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"	
+	echo " 17 Things to Install After installing Ubuntu 17.10"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "1. List all software/utilities"
+	echo "2. Install all software/utilities"
+	echo "3. Contributers"
+	echo "4. Exit"
+}
 
+show_menus_list() {
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"	
+	echo " 17 Things to Install After installing Ubuntu 17.10"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "1. Exit"
+	echo "2. Exit"
+	echo "3. Exit"
+	echo "4. Exit"
+	echo "5. Exit"
+	echo "6. Exit"
+	echo "7. Exit"
+	echo "8. Exit"
+	echo "9. Exit"
+	echo "10. Exit"
+	echo "11. Exit"
+	echo "12. Exit"
+	echo "13. Exit"
+	echo "14. Exit"
+	echo "15. Exit"
+	echo "16. Exit"
+	echo "17. Exit"
+}
 
-sudo add-apt-repository ppa:graphics-drivers/ppa 
-sudo apt-get install libxss1 libappindicator1 libindicator7
-sudo add-apt-repository ppa:dawidd0811/neofetch
-sudo add-apt-repository ppa:vlijm/spaceview
-sudo add-apt-repository ppa:gerardpuig/ppa
-sudo add-apt-repository ppa:shutter/ppa
-sudo add-apt-repository ppa:atareao/atareao
-sudo apt-get install shutter
-sudo apt install national-geographic-wallpaper
-sudo apt-get install git
-sudo apt-get install gitg
-sudo apt-get install synaptic
-sudo apt-get install unity-tweak-tool
-sudo apt install google-chrome-stable
-sudo apt-get install steam
-sudo apt-get install transmission
-sudo apt install gimp gimp-gmic
-sudo apt-get install spaceview
-sudo apt-get install ubuntu-cleaner
-sudo apt-get install filezilla
-sudo apt-get install uget
-sudo apt-get install tor
-sudo apt-get install putty
-sudo apt-get install open-vm-tools-desktop
-sudo apt install android-tools-adb android-tools-fastboot
+show_menus_cont() {
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"	
+	echo " Send all contribution request via Twitter to @TechGameTeddy"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo "All Contributions are greatly appreciated and listed on Github"
+	echo "Tedley Meralus @TechGameTeddy"
+	echo "4. Exit"
+}
 
-git clone https://github.com/ebruck/indicator-doom-cpu/releases/download/v1.0/indicator-doom-cpu-1.0.0-noarch.deb
+#sudo add-apt-repository ppa:graphics-drivers/ppa 
+#sudo apt-get install libxss1 libappindicator1 libindicator7
+#sudo add-apt-repository ppa:dawidd0811/neofetch
+#sudo add-apt-repository ppa:vlijm/spaceview
+#sudo add-apt-repository ppa:gerardpuig/ppa
+#sudo add-apt-repository ppa:shutter/ppa
+#sudo add-apt-repository ppa:atareao/atareao
+#sudo apt-get install shutter
+#sudo apt install national-geographic-wallpaper
+#sudo apt-get install git
+#sudo apt-get install gitg
+#sudo apt-get install synaptic
+#sudo apt-get install unity-tweak-tool
+#sudo apt install google-chrome-stable
+#sudo apt-get install steam
+#sudo apt-get install transmission
+#sudo apt install gimp gimp-gmic
+#sudo apt-get install spaceview
+#sudo apt-get install ubuntu-cleaner
+#sudo apt-get install filezilla
+#sudo apt-get install uget
+#sudo apt-get install tor
+#sudo apt-get install putty
+#sudo apt-get install open-vm-tools-desktop
+#sudo apt install android-tools-adb android-tools-fastboot
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
+#git clone https://github.com/ebruck/indicator-doom-cpu/releases/download/v1.0/indicator-doom-cpu-1.0.0-noarch.deb
 
-sudo apt-get install libxss1 libappindicator1 libindicator7
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome*.deb
+
+#sudo apt-get install libxss1 libappindicator1 libindicator7
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome*.deb
 
 
 ##synaptic package manager 
-sudo apt-get install synaptic
+#sudo apt-get install synaptic
 
 ##Nvidia drivers
-sudo add-apt-repository ppa:graphics-drivers/ppa 
+#sudo add-apt-repository ppa:graphics-drivers/ppa 
 
 ##Unity tweak tool 
-sudo apt-get install unity-tweak-tool
+#sudo apt-get install unity-tweak-tool
 
 ##Install chrome
-sudo apt install google-chrome-stable
+#sudo apt install google-chrome-stable
 
 ##Install steam 
-sudo apt-get install steam
+#sudo apt-get install steam
 
 
 ##Transmission
-sudo apt-get install transmission
+#sudo apt-get install transmission
 
 
 ##Neo fetch(system info via terminal)
-sudo add-apt-repository ppa:dawidd0811/neofetch
-sudo apt-get install neofetch
+#sudo add-apt-repository ppa:dawidd0811/neofetch
+#sudo apt-get install neofetch
 
 #Install Gimp
-sudo apt install gimp gimp-gmic
+#sudo apt install gimp gimp-gmic
 
 
 ##Evolution
-sudo apt-get install evolution
+#sudo apt-get install evolution
 
  
 
 
 
 ##Install Sublime
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
+#sudo add-apt-repository ppa:webupd8team/sublime-text-3
 
 
 ##install Sublime
-sudo apt-get install sublime-text-installer
+#sudo apt-get install sublime-text-installer
 
 
 
@@ -98,32 +142,32 @@ sudo apt-get install sublime-text-installer
 # this one fails
 ## email the developer and tell them 
 ##sudo add-apt-repository ppa:moorkai/cinnamon 
-sudo add-apt-repository ppa:embrosyn/cinnamon
-sudo apt-get install cinnamon
+#sudo add-apt-repository ppa:embrosyn/cinnamon
+#sudo apt-get install cinnamon
 
 ##Nvidia Graphics
-sudo add-apt-repository ppa:graphics-drivers/ppa
-apt-cache search nvidia
+#sudo add-apt-repository ppa:graphics-drivers/ppa
+#apt-cache search nvidia
 
 ##Install SpaceView Indicator
-sudo add-apt-repository ppa:vlijm/spaceview
-sudo apt-get update && sudo apt-get install spaceview
+#sudo add-apt-repository ppa:vlijm/spaceview
+#sudo apt-get update && sudo apt-get install spaceview
 
 ##Install Ubuntu Cleaner
-sudo add-apt-repository ppa:gerardpuig/ppa
-sudo apt-get update && sudo apt-get install ubuntu-cleaner
+#sudo add-apt-repository ppa:gerardpuig/ppa
+#sudo apt-get update && sudo apt-get install ubuntu-cleaner
 
-sudo add-apt-repository ppa:shutter/ppa
-sudo apt-get update
-sudo apt-get install shutter
+#sudo add-apt-repository ppa:shutter/ppa
+#sudo apt-get update
+#sudo apt-get install shutter
 
-National Geographic Wallpaper switcher
-sudo add-apt-repository ppa:atareao/atareao
-sudo apt update && sudo apt install national-geographic-wallpaper
+#National Geographic Wallpaper switcher
+#sudo add-apt-repository ppa:atareao/atareao
+#sudo apt update && sudo apt install national-geographic-wallpaper
 
 
 
-apt-cache search nvidia
+#apt-cache search nvidia
 
 
 
