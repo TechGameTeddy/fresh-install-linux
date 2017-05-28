3a478ac23cfde69f062d896a32ddd8d5e8a4585d
Tedley <tmeralus@gmail.com>
2017-05-28 13:32:56 -0400

    more coming soon

--- fresh-install-linux.sh
+++ fresh-install-linux.sh
@@ -8,11 +8,9 @@
 # check them out at http://www.omgubuntu.co.uk/2017/04/things-to-do-after-installing-ubuntu-17-04
 #
 
-<<<<<<< HEAD
 # Variables 
 STARTOFSCRIPT='date'
 ALLPPA="AllPPA"
-
 INSTALL="sudo apt-get install"
 ADDPPA="sudo add-apt-repository"
 SEARCH="apt-cache search"
@@ -52,159 +50,139 @@ deebweb="tor"
 torrent="transmission"
 dlmanager="uget"
 systemcleaner="ubuntu-cleaner"
-
-
-
 # ----------------------------------
-# Step #2: User defined function
+# learn more menu option
 # ----------------------------------
+learnmore(){
+	echo "learn more" 
+}
+# ----------------------------------
+#  Unattended installer function
+# ----------------------------------
+uinstaller(){
+	echo "Installer started"
+        pause
+}
+# ----------------------------------
+#  Software List Menu functions and Variables
+# ----------------------------------
+## option 1()
+SL_one(){
+	echo "number() called"
+        pause
+}
+## option 2 ()
+SL_two(){
+	echo "number() called"
+        pause
+}
+## Option 3()
+SL_three(){
+	echo "number() called"
+        pause
+}
+## Option 4()
+SL_four(){
+	echo "number() called"
+        pause
+}
+## Option 5 ()
+SL_five(){
+	echo "number() called"
+        pause
+}
+## Option 6 ()
+SL_six(){
+	echo "number() called"
+        pause
+}
+## option 7 ()
+SL_seven(){
+	echo "number() called"
+        pause
+}
+## Option 8()
+SL_eight(){
+	echo "number() called"
+        pause
+}
+## Option 9()
+SL_nine(){
+	echo "number() called"
+        pause
+}
+## Option 10 ()
+SL_ten(){
+	echo "number() called"
+        pause
+}
+## Option 11 ()
+SL_eleven(){
+	echo "number() called"
+        pause
+}
+## option 12 ()
+SL_twelve(){
+	echo "number() called"
+        pause
+}
+## Option 13()
+SL_thirteen(){
+	echo "number() called"
+        pause
+}
+## Option 14()
+SL_fourteen(){
+	echo "number() called"
+        pause
+}
+## Option 15 ()
+SL_fifteen(){
+	echo "number() called"
+        pause
+}
+## Option 16 ()
+SL_sixteen(){
+	echo "number() called"
+        pause
+}
+## option 17 ()
+SL_seventeen(){
+	echo "number() called"
+        pause
+}
+## Option 18()
+SL_eighteen(){
+	echo "number() called"
+        pause
+}
+## Option 19()
+SL_nineteen(){
+	echo "number() called"
+        pause
+}
+## Option 20 ()
+SL_twenty(){
+	echo "number() called"
+        pause
+}
+# ----------------------------------
+#  Return function
+# ----------------------------------
+#pause
 pause(){
   read -p "Press [Enter] key to continue..." fackEnterKey
 }
-# Option one()
-one(){
-	echo "one() called"
-        pause
-}
-=======
-
-
-# function to display menus
-show_menus() {
-	clear
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"	
-	echo " 17 Things to Install After installing Ubuntu 17.10"
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-	echo "1. List all software/utilities"
-	echo "2. Install all software/utilities"
-	echo "3. Contributers"
-	echo "4. Exit"
-}
-
-show_menus_list() {
-	clear
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"	
-	echo " 17 Things to Install After installing Ubuntu 17.10"
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-	echo "1. Exit"
-	echo "2. Exit"
-	echo "3. Exit"
-	echo "4. Exit"
-	echo "5. Exit"
-	echo "6. Exit"
-	echo "7. Exit"
-	echo "8. Exit"
-	echo "9. Exit"
-	echo "10. Exit"
-	echo "11. Exit"
-	echo "12. Exit"
-	echo "13. Exit"
-	echo "14. Exit"
-	echo "15. Exit"
-	echo "16. Exit"
-	echo "17. Exit"
-}
-
-show_menus_cont() {
-	clear
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"	
-	echo " Send all contribution request via Twitter to @TechGameTeddy"
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-	echo "All Contributions are greatly appreciated and listed on Github"
-	echo "Tedley Meralus @TechGameTeddy"
-	echo "4. Exit"
-}
-
-#sudo add-apt-repository ppa:graphics-drivers/ppa 
-#sudo apt-get install libxss1 libappindicator1 libindicator7
-#sudo add-apt-repository ppa:dawidd0811/neofetch
-#sudo add-apt-repository ppa:vlijm/spaceview
-#sudo add-apt-repository ppa:gerardpuig/ppa
-#sudo add-apt-repository ppa:shutter/ppa
-#sudo add-apt-repository ppa:atareao/atareao
-#sudo apt-get install shutter
-#sudo apt install national-geographic-wallpaper
-#sudo apt-get install git
-#sudo apt-get install gitg
-#sudo apt-get install synaptic
-#sudo apt-get install unity-tweak-tool
-#sudo apt install google-chrome-stable
-#sudo apt-get install steam
-#sudo apt-get install transmission
-#sudo apt install gimp gimp-gmic
-#sudo apt-get install spaceview
-#sudo apt-get install ubuntu-cleaner
-#sudo apt-get install filezilla
-#sudo apt-get install uget
-#sudo apt-get install tor
-#sudo apt-get install putty
-#sudo apt-get install open-vm-tools-desktop
-#sudo apt install android-tools-adb android-tools-fastboot
-
-#git clone https://github.com/ebruck/indicator-doom-cpu/releases/download/v1.0/indicator-doom-cpu-1.0.0-noarch.deb
-
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo dpkg -i google-chrome*.deb
-
-#sudo apt-get install libxss1 libappindicator1 libindicator7
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#sudo dpkg -i google-chrome*.deb
-
-
-##synaptic package manager 
-#sudo apt-get install synaptic
-
-##Nvidia drivers
-#sudo add-apt-repository ppa:graphics-drivers/ppa 
-
-##Unity tweak tool 
-#sudo apt-get install unity-tweak-tool
-
-##Install chrome
-#sudo apt install google-chrome-stable
-
-##Install steam 
-#sudo apt-get install steam
-
-
-##Transmission
-#sudo apt-get install transmission
-
-
-##Neo fetch(system info via terminal)
-#sudo add-apt-repository ppa:dawidd0811/neofetch
-#sudo apt-get install neofetch
-
-#Install Gimp
-#sudo apt install gimp gimp-gmic
-
-
-##Evolution
-#sudo apt-get install evolution
-
->>>>>>> 27c9db9aafe411acc67513251bf3bb4a382e409a
- 
-# Option two()
-two(){
-	echo "two() called"
-        pause
-}
-
- # Option three()
-three(){
-	echo "three() called"
-        pause
-}
- 
-# Option four()
-four(){
-	echo "four() called"
-        pause
-}
-# Option five()
-five(){
-	exit
-        pause
+# Press M to return
+M_button(){
+	while true; do
+    read mM
+case $M_button in
+    [mM])
+		showmenus
+#back to main menu
+backtomain(){
+  show_menus
 }
 
 # function to display menus
@@ -228,18 +206,120 @@ show_menus() {
  	echo   "| ##  ##   ### ##    ##    ##    ##     ## ##       ##             |"   
 	echo   "|#### ##    ##  ######     ##    ##     ## ######## ########       |"
 	echo   "|                                                                  |"
-	echo   "| 17 tools/utilities to install after installing Ubuntu 17.10      |"
+	echo   "| Essential tools/utilities to install after installing            |"
+	echo   "| the latest version of Ubuntu                                     |"
 	echo   "| created By @TechGameTeddy                                        |"
 	echo   "|                                                                  |"
 	echo   "| ~~~~~~~~~~~~~~~~~~~~~--------------------------------------------|"
 	echo   "| ~~~~~~~~~~~~~~~~~~~~~--------------------------------------------|"
 	echo   "|1. Run unattended installer                                       |"
-	echo   "|2. update all PPA's                                               |"
+	echo   "|2. Install Dependancies                                           |"
 	echo   "|3. Check Software List                                            |"
 	echo   "|4. Contribute                                                     |"
 	echo   "|5. Exit                                                           |" 
 	echo   "|                                                                  |"
 	echo   "| ~~~~~~~~~~~~~~~~~~~~~--------------------------------------------|"
+
+}
+software_list() {
+	write_header " Package List open-vm-tools-desktop
+"
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"	
+	echo " Essential Tools to install post installing Ubuntu   |" 
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
+	echo "                                                     |"
+	echo "1. Android Device Manager                            |" 
+	echo "2. Chromium                                          |"
+	echo "3. Evolution                                         |"
+	echo "4. Filezilla                                         |"
+	echo "5. Gimp                                              |"
+	echo "6. Git                                               |"
+	echo "7. Inkscape                                          |"
+	echo "8. National-geographic-wallpaper                     |"
+	echo "9. Neofetch                                          |"
+	echo "10.Putty                                             |"
+	echo "11.Shutter                                           |"
+	echo "12.Spaceview                                         |"
+	echo "13.Steam                                             |"
+	echo "14.Synaptic Package Manager                          |"
+	echo "15.Tor                                               |"
+	echo "16.Transmission                                      |"
+	echo "17.Ubuntu System Cleaner                             |"
+	echo "18.Unity Tweak Tool                                  |"
+	echo "19.Visual Studio Code                                |"
+	echo "20.Virtual Box                                      |"
+	echo "                                                     |"
+	echo "      type learn# to learn more about each item      |"  
+	echo "       or press M to go back to the main menu        |"
+	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
+
+}
+contributors() {
+	write_header " Contributors "
+	clear
+	echo "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
+	echo "|      ~~|~~|  |  /\  |\  || /  \   //~~\ |   |                    |"
+	echo "|        |  |--| /__\ | \ ||(    \ /|    ||   |                    |"
+	echo "|        |  |  |/    \|  \|| \    |  \__/  \_/                     |"
+	echo "|                                                                  |"	
+	echo "|                                                                  |"
+	echo "|  All Contributions are greatly appreciated and listed here       |" 
+	echo "|                                                                  |"
+	echo "|  1.Tedley Meralus @TechGameTeddy                                 |"
+	echo "|  2.Could be you                                                  |"
+	echo "|  3.Someone else not as great as you                              |"
+	echo "|                                                                  |"
+	echo "|  Send all contribution request via Twitter to @TechGameTeddy     |"
+	echo "|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|"
+	pause
+}
+# ----------------------------------
+#  Main Menu Option
+# ----------------------------------
+
+## Option one Unattended installer ()
+one(){
+	while true; do
+    read -p "Are you sure you want to run the unattended installer?" input
+case $input in
+    [yY][eE][sS]|[yY])
+		uinstaller
+		;;
+ 
+    [nN][oO]|[nN])
+		backtomain
+		break
+       		;;
+    *)
+	echo "Invalid input..."
+	echo "Please type (y)Yes or (n)No" 
+	;;
+esac
+done
+        pause
+}
+## Option two Update PPA List ()
+two(){
+	echo "two() called"
+        pause
+}
+
+## Option three Check Software List()
+three(){
+	software_list
+        pause
+}
+ 
+## Option four Contributors()
+four(){
+	contributors
+
+}
+## Option five Exit ()
+five(){
+	exit
+        pause
 }
 # read input from the keyboard and take a action
 # invoke the numbers() when the user select an option from the menu option.
@@ -265,56 +345,5 @@ do
  
 	show_menus
 	read_options
+
 done
-
-
-
-<<<<<<< HEAD
-=======
-##Install Sublime
-#sudo add-apt-repository ppa:webupd8team/sublime-text-3
-
-
-##install Sublime
-#sudo apt-get install sublime-text-installer
-
-
-
-##Install Cinnamon
-# this one fails
-## email the developer and tell them 
-##sudo add-apt-repository ppa:moorkai/cinnamon 
-#sudo add-apt-repository ppa:embrosyn/cinnamon
-#sudo apt-get install cinnamon
-
-##Nvidia Graphics
-#sudo add-apt-repository ppa:graphics-drivers/ppa
-#apt-cache search nvidia
-
-##Install SpaceView Indicator
-#sudo add-apt-repository ppa:vlijm/spaceview
-#sudo apt-get update && sudo apt-get install spaceview
-
-##Install Ubuntu Cleaner
-#sudo add-apt-repository ppa:gerardpuig/ppa
-#sudo apt-get update && sudo apt-get install ubuntu-cleaner
-
-#sudo add-apt-repository ppa:shutter/ppa
-#sudo apt-get update
-#sudo apt-get install shutter
-
-#National Geographic Wallpaper switcher
-#sudo add-apt-repository ppa:atareao/atareao
-#sudo apt update && sudo apt install national-geographic-wallpaper
-
-
-
-#apt-cache search nvidia
-
-
->>>>>>> 27c9db9aafe411acc67513251bf3bb4a382e409a
-
-
-
-
-
