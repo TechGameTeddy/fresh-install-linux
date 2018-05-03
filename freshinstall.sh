520296de4979be49be291fd8192f25414f7b6aa3
tmeralus <tmeralus@gmail.com>
2018-05-03 12:45:13 -0400

    updated menus

--- freshinstall.sh
+++ freshinstall.sh
@@ -1,8 +1,8 @@
 # Name: Fresh Install
 # Author: Tedley Meralus
 # Contact: @TechGameTeddy
-# Menu driven script to automate and download a list of popular tools and utilities
-# to install on a Linux desktop
+# Menu driven script to backup and restore files from an Ubuntu desktop
+# add to your github channel for personal use
 # inspired by OMG Ubuntu's infamous
 # Top Things to do after Installing Ubuntu xx.xx articles
 # check them out at http://www.omgubuntu.co.uk/2017/04/things-to-do-after-installing-ubuntu-17-04
@@ -12,23 +12,11 @@ STARTOFSCRIPT='date'
 #ALLPPA="AllPPA"
 INSTALL="sudo apt-get install "
 ADDPPA="sudo add-apt-repository "
-ALLPPA="ppa:dawidd0811/neofetch ppa:vlijm/spaceview ppa:gerardpuig/ppa ppa:shutter/ppa ppa:atareao/atareao"
 SEARCH="apt-cache search "
 SHOW="apt-cache showpkg "
-#UPDATE="apt-get udpate"
+UPDATE="apt-get udpate"
 PPADIR="/etc/apt/sources.list/"
 PPADIR2="/etc/apt/sources.list.d/"
-fre
-# Variables to install all available ppa's
-nvidia="ppa:graphics-drivers/ppa" #Nvidia ppa
-neoppa="ppa:dawidd0811/neofetch" #neofetch ppa
-sviewppa="ppa:vlijm/spaceview" #spaceview ppa
-gerardpuig="ppa:gerardpuig/ppa" #ubuntu cleaner ppa
-shutppa="ppa:shutter/ppa" #Shutter screenshot ppa
-atareaoppa="ppa:atareao/atareao" #national-geographic-wallpaper ppa
-
-#Variables for apps being installed
-ALLAPPS="android-tools-adb android-tools-fastboot chromium-browser docker elinks filezilla gimp git coreutils inkscape libappindicator1 libindicator7 evolution national-geographic-wallpaper neofetch npm playonlinux putty shutter steam synaptic terminator tor transmission ubuntu-cleaner unity-tweak-tool uget vagrant virtualbox open-vm-tools-desktop"
 # ----------------------------------
 # function to display menus
 # ----------------------------------
@@ -39,107 +27,176 @@ show_menus() {
 	echo "|--------------------------------------------------------"
 	echo "|         FRESH INSTALL                                  "
 	echo "|--------------------------------------------------------"
-	echo "| Essential tools/utilities to install 				   "
-	echo "| after installing the latest version of Ubuntu          "
+	echo "| Essential tools/utilities to install 		       "
+	echo "| after installing or crashing your version of Ubuntu    "
 	echo "| created By @TechGameTeddy                              "
 	echo "|                                                        "
-	echo "|1. Run unattended installer                             "
-	echo "|2. Install Dependancies                                 "
-	echo "|3. Check Software List                                  "
-	echo "|5. Exit                                                 "
+	echo "|1. Run Fresh Install                                    "
+	echo "|2. Run Backup Utility                                   "
+	echo "|3. Check Software List(s)                               "
+	echo "|4. Exit                                                 "
 	echo "|--------------------------------------------------------"
 }
-software_list() {
-	write_header " Package List "
+software_backup() {
+	write_header " Backup utility "
+	clear
+	date
+	echo "|--------------------------------------------------------"
+	echo "|--------------------------------------------------------"
+	echo "|         Backup Utility                                 "
+	echo "|--------------------------------------------------------"
+	echo "| Essential tools/utilities to install 	               "
+	echo "| after installing or crashing your version of Ubuntu    "
+	echo "| created By @TechGameTeddy                              "
+	echo "|                                                        "
+	echo "|1. list  pacakges                                       "
+	echo "|2. list  snaps                                          " 
+	echo "|3. check sources.d list                                 "
+	echo "|4. Install software list(s)                             "
+	echo "|5. add to software list                                 "
+	echo "|6. Exit                                                 "
+	echo "|--------------------------------------------------------"
+}
+fresh_install() {
+	write_header " Fresh Install utility "
 	clear
 	echo "|-----------------------------------------------------------------"
 	echo "|  Essential Tools to install post installing Ubuntu              "
 	echo "|-----------------------------------------------------------------"
-	echo "|	001.Android Device Manager                                      "
-	echo "|	002.Chromium                                                    "
-	echo "|	003.Filezilla                                                   "
-	echo "|	004.Gimp                                                        "
-	echo "|	005.Git                                                         "
-	echo "|	006.GNU Coreutils                                               "
-	echo "|	007.Inkscape                                                    "
-	echo "|	008.Putty                                                       "
-	echo "| 009.Shutter                                                     "
-	echo "|	010.Steam                                                       "
-	echo "|	011.Synaptic Package Manager                                    "
-	echo "|	012.Tor                                                         "
-	echo "|	013.Transmission                                                "
-	echo "|	014.Ubuntu System Cleaner                                       "
-	echo "|	015.Unity Tweak Tool                                            "
-	echo "| 016.Visual Studio Code                                          "
-	echo "| 017.Virtual Box                                                 "
-	echo "|  press "m" to go back to the main menu                          "
+	echo "|1. list  pacakges                                                "
+	echo "|2. list  snaps                                                   "
+	echo "|3. check sources.d list                                          "
+	echo "|5. add to software list                                          "
+	echo "|4. Install software list(s)                                      "
+	echo "|4. Run Full Install                                              "
+	echo "|6. Back to Main                                                  "
+	echo "|-----------------------------------------------------------------"
 	echo "| ----------------------------------------------------------------"
 	sl_options
 }
-# ----------------------------------
-#  UNATTENDED INSTALLER FUNCTION
-# ----------------------------------
-uinstaller(){
-	echo "Installer started"
-	echo "Downloading Dependancies"
-	sudo apt-add-repository ${ALLPPA[*]}
-	echo "All Dependancies installed"
-	echo "Updating system now"
-	$UPDATE
-	echo "Update complete"
-	echo "Installing all packages"
-	sudo apt-get install ${ALLAPPS[*]}
-        pause
+sl_lists() {
+	write_header " Software Lists "
+	clear
+	echo "|-----------------------------------------------------------------"
+	echo "|                       Software Lists                            "
+	echo "|-----------------------------------------------------------------"
+	echo "|1. Desktop List                                                  "
+	echo "|2. Developer List                                                "
+	echo "|3. Sysadmin List                                                 "
+	echo "|4. Create New List                                               "
+	echo "|5. Check Backup Lists                                            "
+	echo "|6. Back to Main                                                  "
+	echo "|-----------------------------------------------------------------"
+	echo "| ----------------------------------------------------------------"
+	sl_options
 }
-
-# ----------------------------------
-#  Return functions
-# ----------------------------------
-pause(){
-  read -p "Press [Enter] key to continue..." fackEnterKey
-}
-# ----------------------------------
-#back to main menu
-# ----------------------------------
-backtomain(){
-  show_menus
-}
-# ----------------------------------
+---------------------------------
 #  Main Menu Option
 # ----------------------------------
 one(){
-	while true; do
-	read -p "Are you sure you want to run the unattended installer?[y/n]" input
-	case $input in
-	[yY][eE][sS]|[yY])
-		uinstaller
-		;;
-	[nN][oO]|[nN])
-		backtomain
-       		break;;
-    *)
-	echo "Invalid input..."
-	echo "Please type (y)Yes or (n)No"
-	;;
-esac
-done
         pause
 }
 ## Option two Update PPA List ()
 two(){
-	sudo apt-add-repository ${ALLPPA[*]}
-        pause
+
 }
 ## Option three Check Software List()
 three(){
-	software_list
+	#software_list
         #pause
 }
 ## Option four Exit ()
 four(){
-	clear
+	#clear
 	exit
 }
+---------------------------------
+#  Backup Utility Options SB1 
+# ----------------------------------
+SB1(){
+        pause
+}
+## Option two Update PPA List ()
+SB2(){
+
+}
+## Option three Check Software List()
+SB3(){
+	#software_list
+        #pause
+}
+## Option four Exit ()
+SB4(){
+	#clear
+	exit
+}
+SB5(){
+	#clear
+	exit
+}
+SB6(){
+	#clear
+	exit
+}
+---------------------------------
+#  Fresh Install Utility Options FI1
+# ----------------------------------
+FI1(){
+        pause
+}
+## Option two Update PPA List ()
+FI2(){
+
+}
+## Option three Check Software List()
+FI3(){
+	#software_list
+        #pause
+}
+## Option four Exit ()
+FI4(){
+	#clear
+	exit
+}
+FI5(){
+	#clear
+	exit
+}
+FI6(){
+	#clear
+	exit
+}
+
+---------------------------------
+#  Software List Utility Options SOL1
+# ----------------------------------
+SOL1(){
+        pause
+}
+## Option two Update PPA List ()
+SOL2(){
+
+}
+## Option three Check Software List()
+SOL3(){
+	#software_list
+        #pause
+}
+## Option four Exit ()
+SOL4(){
+	#clear
+	exit
+}
+SOL5(){
+	#clear
+	exit
+}
+SOL6(){
+	#clear
+	exit
+}
+
+
 # ----------------------------------
 #  MAIN MENU
 # ----------------------------------
@@ -151,44 +208,77 @@ read_options(){
 		2) two    ;;
 		3) three  ;;
 		4) four   ;;
-		*) echo -e "${RED}Error...${STD}" && sleep 2
+		*) echo -e "${RED}Error...${STD}" && sleep 1
 	esac
 }
 # ----------------------------------
 #  SOFTWARE LIST MENU
 # ----------------------------------
-sl_options(){
-	local softchoice
-	read -p "Enter choice [ 001 - 020] " softchoice
-	case $softchoice in
-		001) SL1  ;;
-		002) SL2  ;;
-		003) SL3  ;;
-		004) SL4  ;;
-		005) SL5  ;;
-		006) SL6  ;;
-		007) SL7  ;;
-		008) SL8  ;;
-		009) SL9  ;;
-		010) SL10 ;;
-		011) SL11 ;;
-		012) SL12 ;;
-		013) SL13 ;;
-		014) SL14 ;;
-		015) SL15 ;;
-		016) SL16 ;;
-		017) SL17 ;;
-		018) SL18 ;;
-		019) SL19 ;;
-		020) SL20 ;;
-	[mM])
+software_backup_list(){
+	local sblchoice
+	read -p "Enter choice [ 001 - 006] " sblchoice
+	case $sblchoice in
+		1) SB1  ;;
+		2) SB2  ;;
+		3) SB3  ;;
+		4) SB4  ;;
+		5) SB5  ;;
+	[qQ])
 		show_menus
        		;;
-		*) echo -e "${RED}Invalid option choose [ 001 - 020] or "m" to quit...${STD}" && sleep 2
+		*) echo -e "${RED}Invalid option choose [ 1 - 6] or "q" to quit...${STD}" && sleep 2
        		clear
 		software_list
 		       ;;
 esac
+}
+fresh_install_list(){
+	local filchoice
+	read -p "Enter choice [ 001 - 006] " filchoice
+	case $filchoice in
+		1) FI1  ;;
+		2) FI2  ;;
+		3) FI3  ;;
+		4) FI4  ;;
+		5) FI5  ;;
+	[qQ])
+		show_menus
+       		;;
+		*) echo -e "${RED}Invalid option choose [ 1 - 6] or "q" to quit...${STD}" && sleep 2
+       		clear
+		software_list
+		       ;;
+esac
+}
+sl_option_list(){
+	local solchoice
+	read -p "Enter choice [ 001 - 006] " solchoice
+	case $solchoice in
+		1) SOL1  ;;
+		2) SOL2  ;;
+		3) SOL3  ;;
+		4) SOL4  ;;
+		5) SOL5  ;;
+	[qQ])
+		show_menus
+       		;;
+		*) echo -e "${RED}Invalid option choose [ 1 - 6] or "q" to quit...${STD}" && sleep 2
+       		clear
+		software_list
+		       ;;
+esac
+}
+# ----------------------------------
+#  Return functions
+# ----------------------------------
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
+# ----------------------------------
+#back to main menu
+# ----------------------------------
+backtomain(){
+  show_menus
 }
 # ----------------------------------
 # Trap CTRL+C, CTRL+Z and quit singles
@@ -204,3 +294,5 @@ do
 	read_options
 	date_and_time
 done
+
+
---
+++ guiscript.sh
@@ -0,0 +1,109 @@
+# Name: Sample Gui for Bash Scripts
+# Author: Tedley Meralus
+# Contact: @TechGameTeddy
+# written with traps, signals and loops
+#
+#
+#Variables for apps being installed
+ALLVARS=""
+# function to display menus
+# ----------------------------------
+show_menus() {
+	clear
+	date
+	echo "|-----------------------------------------------------------------------------------"
+	echo "|-----------------------------------------------------------------------------------"
+	echo "|------------------------Sample Gui for Bash Scripts------------------------------- "
+	echo "|-------------------written with traps, signals and loops-------------------------- "
+	echo "|-----------------------------------------------------------------------------------"
+	echo "|-----------------------------------------------------------------------------------"
+	echo "|-----------------------------------------------------------------------------------"
+	echo "| Redis Development Setup             	        	                                  "
+	echo "| For Development purposes                                                          "
+	echo "| created By @TechGameTeddy                                                         "
+	echo "|                                                                                   "
+	echo "|1.Install Redis4.0.9                                                               "
+	echo "|2.Check if Redis is installed                                                      "
+	echo "|3.Check Check Redis Ports                                                          "
+	echo "|4.Check All Redis log locations                                                    "
+	echo "|5.Check Check Redis Ports                                                          "
+	echo "|10.Exit                                                                            "
+	echo "|-----------------------------------------------------------------------------------"
+}
+
+
+# ----------------------------------
+#  Return functions
+# ----------------------------------
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
+# ----------------------------------
+#back to main menu
+# ----------------------------------
+backtomain(){
+  show_menus
+}
+# ----------------------------------
+#  Main Menu Option
+# ----------------------------------
+## Option one
+one(){
+
+}
+
+## Option two
+two(){
+}
+
+## Option three
+three(){
+}
+
+## Option four Exit ()
+four(){
+
+}
+## Option five
+five(){
+
+}
+## Option for Exit ()
+exit(){
+
+}
+
+# ----------------------------------
+#  MAIN MENU
+# ----------------------------------
+read_options(){
+	local choice
+	read -p "Enter your choice [ 1 - 4] " choice
+	case $choice in
+		1) one    ;;
+		2) two    ;;
+		3) three  ;;
+		4) four   ;;
+		4) five   ;;
+		4) six   ;;
+		4) seven   ;;
+		4) eight   ;;
+		4) nine   ;;
+		4) ten   ;;
+		*) echo -e "${RED}Error...${STD}" && sleep 2
+	esac
+}
+# ----------------------------------
+# Trap CTRL+C, CTRL+Z and quit singles
+# ----------------------------------
+trap '' SIGINT SIGQUIT SIGTSTP
+
+# ----------------------------------
+# Main Menu logic - infinite loop
+# ----------------------------------
+while true
+do
+	show_menus
+	read_options
+	date_and_time
+done
